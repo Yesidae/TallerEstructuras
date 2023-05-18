31e04c229a91e1444555f774a3b4902d18d40431
Yesidae <yesidae.1070@gmail.com>
2023-05-17 21:19:00 -0500

    Actualizaciones

---
+++ TallerEstructuras/Taller.Logic/Exercices.cs
@@ -0,0 +1,82 @@
+﻿namespace Taller.Logic
+{
+    public class Exercices
+    {
+
+        public static int[,] GetMatrixMultiplication(int m, int n, int p)
+        {
+            int[,] A = new int[m, n];
+            int[,] B = new int[n, p];
+            int[,] C = new int[m, p];
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    A[i, j] = (i + 1) * (j);
+                }
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    B[i, j] = (j + 1) * (i);
+                }
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < p; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        C[i, j] += A[i, k] * B[k, j];
+                    }
+                }
+            }
+
+            return C;
+        }
+
+
+        public static Queue<int> DecompositionInFactors(int number)
+        {
+            var factors = new Queue<int>();
+            int div = 2;
+
+            while (number > 1)
+            {
+                if (number % div == 0)
+                {
+                    factors.Enqueue(div);
+                    number /= div;
+                }
+                else
+                {
+                    div++;
+                }
+            }
+            return factors;
+        }
+
+        public static int[,] Hourglass(int n)
+        {
+
+
+            return null!;
+
+        }
+
+        public static string HarvestInHorse(char posCrops, char posHorse)
+        {
+            char[,] board = new char[8, 8];
+            var field = new Dictionary<char, int>();
+
+            field.Add(posCrops, 1);
+
+            
+            return null!;
+        }
+    }
+}
--- TallerEstructuras/Taller.Logic/Program.cs
+++ TallerEstructuras/Taller.Logic/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Taller.Logic;
 
-namespace Taller.Logic
+Console.WriteLine("****Matrix Multiplicatión****");
+
+
+Console.WriteLine("****Descomposition Into Factors****");
+
+Console.Write("Enter the number to descompose");
+int number = int.Parse(Console.ReadLine()!);
+
+Queue<int> factors = Exercices.DecompositionInFactors(number);
+
+Console.WriteLine($"Factors descompose is {number} : ");
+while (factors.Count > 0)
 {
-    internal class Program
-    {
-    }
+    Console.WriteLine($"{factors.Dequeue()} ");
 }
+Console.WriteLine();
+
+
+Console.WriteLine("****Descomposition Into Factors****");
+
+Console.Write("Ingrese la ubicación de los frutos: ");
+char frutos = char.Parse(Console.ReadLine()!);
+
+Console.Write("Ingrese la ubicación del caballo: ");
+char caballo = char.Parse(Console.ReadLine()!);
