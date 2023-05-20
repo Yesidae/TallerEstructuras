0f9a591c33e41bc6bafc333cbf4e96f373e98307
Yesidae <yesidae.1070@gmail.com>
2023-05-19 19:18:11 -0500

    HorseHarvest

--- TallerEstructuras/HarvestInHorse.Logic/Harvest.cs
+++ TallerEstructuras/HarvestInHorse.Logic/Harvest.cs
@@ -2,15 +2,112 @@
 {
     public class Harvest
     {
-        public static string HarvestInHorse(char posCrops, char posHorse)
+
+        public string Recolects { get; set; }
+
+        public string Fruits { get; set; }
+
+        public string Moves { get; set; }
+
+        public string PosHorse { get; set; }
+
+        public Dictionary<string, int> Frts { get; set; }
+
+        public Harvest(string fruits, string posHorse, string moveHorse)
         {
-            char[,] board = new char[8, 8];
-            var field = new Dictionary<char, int>();
-
-            field.Add(posCrops, 1);
-
-
-            return null!;
+            Fruits = fruits;
+            PosHorse = posHorse;
+            Moves = moveHorse;
+            Frts = new Dictionary<string, int>();
+            SplitText();
+            Recolects = CollectedFruits();
         }
+
+        public static string MoveHorse(string pos, string moves)
+        {
+            int x = pos[0] - 'A';
+            int y = pos[1] - '1';
+
+            switch (moves)
+            {
+                case "UL":
+                    x -= 1;
+                    y += 2;
+                    break;
+
+                case "UR":
+                    x += 1;
+                    y += 2;
+                    break;
+
+                case "LU":
+                    x -= 2;
+                    y += 1;
+                    break;
+
+                case "LD":
+                    x -= 2;
+                    y -= 1;
+                    break;
+
+                case "RU":
+                    x += 2;
+                    y += 1;
+                    break;
+
+                case "RD":
+                    x += 2;
+                    y -= 1;
+                    break;
+
+                case "DL":
+                    x -= 1;
+                    y -= 2;
+                    break;
+
+                case "DR":
+                    x += 1;
+                    y -= 2;
+                    break;
+            }
+            return "" + (char)('A' + x) + (char)('1' + y);
+        }
+
+
+        
+
+        public void SplitText()
+        {
+            var fruits = Fruits.Split(',');
+            foreach (string aux in fruits)
+            {
+                string position = aux[..2];
+                char fruit = aux[2];
+                this.Frts[position] = fruit;
+            }
+        }
+
+        public string CollectedFruits()
+        {
+            var collec = "";
+            var moves = Moves.Split(",");
+
+            foreach (var move in moves)
+            {
+                PosHorse = MoveHorse(PosHorse, move);
+
+                if (Frts.ContainsKey(PosHorse))
+                {
+                    int number = int.Parse(Frts[PosHorse].ToString());
+                    char words = Convert.ToChar(number);
+                    collec += words + " ";
+                    Frts.Remove(PosHorse);
+                }
+            }
+
+            return $"The collect is: {collec}";
+        }
+
+        
     }
 }
--- TallerEstructuras/HarvestInHouse.UI/Program.cs
+++ TallerEstructuras/HarvestInHouse.UI/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HarvestInHorse.Logic;
+
+Console.WriteLine("****Harvest In Horse*****");
+
+Console.WriteLine();
+
+
+Console.Write("Ingrese la ubicación de los frutos: ");
+var cultive = Console.ReadLine();
+
+Console.Write("Ingrese la ubicación inicial del caballo: ");
+var posHorse = Console.ReadLine()!;
+
+Console.Write("Ingrese los movimientos del caballo: ");
+var moves = Console.ReadLine();
+
+
+var harvest = new Harvest(cultive, posHorse, moves);
+
+Console.Write(harvest.Recolects);
